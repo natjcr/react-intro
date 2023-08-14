3076c84886ef33f6e87ba2b28faad916d47fc8da
njcre <njcollazosr@gmaile.com>
2023-08-14 17:01:25 -0500

    new3.1

--- react-intro/src/TodoSearch/TodoSearch.jsx
+++ react-intro/src/TodoSearch/TodoSearch.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
 import './TodoSearch.css'
 
-function TodoSearch() {
+function TodoSearch() { //estado inmutable
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('usuarios buscan ' + searchValue)
+
     return(
       <input className='TodoSearch'
-      placeholder='Tareas Pendientes' />
+        placeholder='Tareas Pendientes' 
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value)
+      }}
+      />
     );
   }
 
